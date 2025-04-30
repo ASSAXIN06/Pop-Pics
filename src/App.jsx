179ba23a640ef8afce05a278bab8200e85ca7a38
ASSAXIN06 <aniketsprofessionalid@gmail.com>
2025-04-30 11:56:58 +0530

    starting from sqaure Zero and add MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,12 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
-import Search from './components/search'
 
 function App() {
 
-  const [searchTerm, setSearchTerm] = useState('');
-
   return (
-    <main>
-      <div className='pattern'/>
-
-    
-      <div className='wrapper'>
-        <header>
-          <img src="./hero.png" alt="Hero Banner"/>
-          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
-        </header>
-
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      </div>
-    </main>
+    <>
+    <h1>Movies are here</h1>
+    </>
   )
 }
 
