efc509f0759dc62f825470ac64f655c16d8946fb
ASSAXIN06 <aniketsprofessionalid@gmail.com>
2025-04-30 16:47:57 +0530

    add Home page to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState,useEffect } from 'react'
 import './App.css'
 import MovieCard from './components/MovieCard'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <MovieCard movie={{title: "Aniket's Film", release_date: "2025"}}/>
+      < Home />
     </>
   )
 }
